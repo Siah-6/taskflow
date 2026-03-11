6bb5ca4a3839da939692e0921b605ef812d11991
Siah-6 <apacionado.siah@gmail.com>
2026-03-11 23:21:40 +0800

    implement authentication (register, login, logout) with bcrypt and JWT

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./config/db.js";
 import { fileURLToPath } from "url";
 import path from "path";
@@ -16,6 +17,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("TaskFlow API running");
